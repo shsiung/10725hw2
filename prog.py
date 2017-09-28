d2870b10603514505a8d25e8ff8c24d913a5b8ab
Jerry Hsiung <shsiung@g.hmc.edu>
2017-09-27 23:35:55 -0400

    before mod

--- prog.py
+++ prog.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
-from sklearn import mixture
 
 def readFile(filename):
     vectors = []
@@ -17,9 +16,9 @@ def objective_value(w,x,y,C,lambda_):
     loss_1 = 1.0/2*np.sum(w*w)
     loss_2 = 0;
 
-    for i in range(len(x)):
+    for i in range(n):
         for j in range(C):
-            loss_2 += pow(loss_function_wj(w.transpose()[j],x[i],y[i],j+1),2)
+            loss_2 += pow(loss_function_wj(w[j],x[i],y[i],j+1),2)
     loss_2 *= lambda_/n
 
     return loss_1 + loss_2
@@ -36,13 +35,11 @@ def evaluate_gradient(w,x,y,C,lambda_,j_):
     zeros = np.zeros(shape=(m)).tolist()
 
     grad_1 = w[j_]
-    print grad_1.shape
     grad_2 = np.zeros(shape=(m))
     grad_2_scale = 0;
-    for i in range(len(x)):
-        grad_2_scale = 0;
-        for j in range(C):
-            grad_2_scale += loss_function_wj(w[j],x[i],y[i],j+1)
+    for i in range(n):
+        dgrad_2_scale = 0;
+        grad_2_scale += loss_function_wj(w[j],x[i],y[i],j+1)
 
         if y[i]==j_+1:
             grad_2 += grad_2_scale * np.maximum(zeros, [ -x_i for x_i in x[i].tolist()])
@@ -66,28 +63,31 @@ def main():
     train_Y = np.array(temp_Y).astype('int');
 
     C = max(train_Y);
-    n = len(train_X)        # number of data
-    m = train_X.shape[1]; # size of feature
+    n = len(train_X);       # number of data
+    m = train_X.shape[1];   # size of feature
+    print "n: " + str(n);
+    print "m: " + str(m);
 
     learning_rate = 0.005;
     lambda_ = 0.1;
     epoch = 20;
 
     w = np.zeros(shape=(m,C));
-    print "== Initial loss: " + str(objective_value(w,train_X,train_Y,C,lambda_))
-
-    batch_X = train_X[0:20];
-    batch_Y = train_Y[0:20];
+    print "== Initial loss: " + str(objective_value(w.transpose(),train_X,train_Y,C,lambda_))
+    mini_train_X = train_X
+    mini_train_Y = train_Y
 
     for k in range(epoch):
+        np.random.shuffle(mini_train_X)
+        np.random.shuffle(mini_train_Y)
+        batch_X = mini_train_X[0:5000]
+        batch_Y = mini_train_Y[0:5000]
         for j in range(C):
             param_grad = evaluate_gradient(w.transpose(),batch_X,batch_Y,C,lambda_,j)
             w.transpose()[j] = w.transpose()[j] - learning_rate * param_grad
 
-        print " -> Epoch " + str(k) + " with loss " + str(objective_value(w,train_X,train_Y,C,lambda_))
+        print " -> Epoch " + str(k) + " with loss " + str(objective_value(w.transpose(),train_X,train_Y,C,lambda_))
         pred_y = np.array(pred(w,train_X))
-        print pred_y[0:10]
-        print train_Y.transpose()[0:10]
         print acc(pred_y,train_Y)
 
 if __name__ == '__main__':
